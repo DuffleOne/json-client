ffad520e883a2b15aedbcc54eb31e4a60346ae43
George Miller <git@duffleman.co.uk>
2021-07-08 08:47:20 +0100

    version 2

--- src/JSONClient.php
+++ src/JSONClient.php
@@ -2,166 +2,35 @@
 
 namespace Duffleman\JSONClient;
 
-use Duffleman\JSONClient\Collections\CollectionManager;
-use Duffleman\JSONClient\Exceptions\JSONError;
-use Duffleman\JSONClient\Exceptions\JSONLibraryException;
-use function Duffleman\JSONClient\decode;
-use function Duffleman\JSONClient\encode;
 use GuzzleHttp\Client;
-use GuzzleHttp\Exception\BadResponseException;
+use GuzzleHttp\Psr7\Request;
 
 /**
  * Class JSONClient.
  */
 class JSONClient
 {
-    protected static $version = '1.0.3';
-
-    /**
-     * Holds the root Guzzle client we work on top of.
-     *
-     * @var Client
-     */
+    protected static string $version = '2.0.0';
     protected $client;
-
-    /**
-     * Any global headers we care about.
-     * In this case, we ALWAYS want a JSON response where possible.
-     *
-     * @var array
-     */
-    protected $global_headers = [
+    protected float $timeout = 10;
+    protected array $global_headers = [
         'Accept' => 'application/json',
     ];
 
-    /**
-     * The base timeout for requests.
-     *
-     * @var float
-     */
-    protected $timeout = 10.0;
-
-    /**
-     * Should the response return as:
-     * -1: Response() object from Guzzle.
-     * 0: String
-     * 1: Array
-     * 2: Generic/Collection.
-     *
-     * @var bool
-     */
-    protected $mode = 2;
-
-    /**
-     * JSONClient constructor.
-     *
-     * @param string $base_url
-     * @param array  $headers
-     */
-    public function __construct($base_url = '', array $headers = [], $timeout = 10)
+    public function __construct(string $base_url, array $opts = [])
     {
-        $opts = [];
-        $opts['timeout'] = $this->timeout = $timeout;
+        $opts['base_uri'] = $base_url;
 
         $this->global_headers['User-Agent'] = \GuzzleHttp\default_user_agent().' json-client/'.self::$version;
-        $this->global_headers = array_merge($this->global_headers, $headers);
 
-        if (!empty($base_url)) {
-            $opts['base_uri'] = $base_url;
+        if (array_key_exists('headers', $opts)) {
+            $this->global_headers = array_merge($this->global_headers, $opts['headers']);
         }
 
         $this->client = new Client($opts);
     }
 
-    /**
-     * Special method for GET because we never pass in a body.
-     *
-     * @param string $url
-     * @param array  $query
-     * @param array  $headers
-     *
-     * @return Collections\Generic|\Illuminate\Support\Collection|void
-     */
-    public function get($url, $query = [], $headers = [])
-    {
-        $headers = array_merge($this->global_headers, $headers);
-
-        return $this->request('GET', $url, [], $query, $headers);
-    }
-
-    /**
-     * The main meaty request method, handles
-     * all outgoing requests and deals with responses.
-     *
-     * @param string $method
-     * @param string $url
-     * @param array  $body
-     * @param array  $query
-     * @param array  $headers
-     *
-     * @throws JSONError
-     * @throws JSONLibraryException
-     *
-     * @return Collections\Generic|\Illuminate\Support\Collection|null|void
-     */
-    public function request($method, $url, $body = [], $query = [], $headers = [])
-    {
-        list($body, $query, $headers) = $this->setupVariables($body, $query, $headers);
-
-        if (!empty($body)) {
-            $body = encode($body);
-            $headers['Content-Type'] = 'application/json';
-        } else {
-            $body = null;
-        }
-
-        $headers = array_merge($this->global_headers, $headers);
-
-        try {
-            $response = $this->client->request($method, $url, [
-                'query'   => $query,
-                'body'    => $body,
-                'headers' => $headers,
-            ]);
-            $response_body = (string) $response->getBody();
-        } catch (BadResponseException $exception) {
-            self::handleError($exception);
-        }
-
-        switch ($this->mode) {
-            case -1:
-                return $response;
-            case 0:
-                return $response_body;
-            case 1:
-                if (!empty($response_body)) {
-                    return decode($response_body);
-                }
-
-                return;
-            case 2:
-                if (!empty($response_body)) {
-                    return CollectionManager::build(decode($response_body));
-                }
-
-                return;
-            default:
-                throw new JSONLibraryException('unknown_mode_set');
-        }
-    }
-
-    /**
-     * Return a promise for async requests.
-     *
-     * @param string $method
-     * @param string $url
-     * @param array  $body
-     * @param array  $query
-     * @param array  $headers
-     *
-     * @return \GuzzleHttp\Promise\PromiseInterface
-     */
-    public function requestAsync($method, $url, $body = [], $query = [], $headers = [])
+    public function request(string $method, string $url, array $body = [], array $query = [], array $headers = [])
     {
         if (!empty($body)) {
             $body = encode($body);
@@ -172,123 +41,20 @@ class JSONClient
 
         $headers = array_merge($this->global_headers, $headers);
 
-        return $this->client->requestAsync($method, $url, [
-            'query'   => $query,
-            'body'    => $body,
+        $response = $this->client->request($method, $url, [
+            'query' => $query,
+            'body' => $body,
             'headers' => $headers,
         ]);
-    }
 
-    /**
-     * Handles the error for us, just a bit of code abstraction.
-     *
-     * @param BadResponseException $exception
-     *
-     * @throws JSONError
-     */
-    public static function handleError(BadResponseException $exception)
-    {
-        $response_body = (string) $exception->getResponse()->getBody();
-        $array_body = decode($response_body);
+        $response_body = (string) $response->getBody();
 
-        $code = $exception->getResponse()->getStatusCode();
+        $out = json_decode($response_body);
 
-        $lookForKeys = ['message', 'code', 'error'];
-
-        foreach ($lookForKeys as $key) {
-            if (isset($array_body[$key])) {
-                $message = $array_body[$key];
-            }
+        if (JSON_ERROR_NONE === json_last_error()) {
+            return $out;
         }
 
-        if (!$message) {
-            $message = 'unknown_error';
-        }
-
-        throw new JSONError($message, $code, $array_body);
-    }
-
-    /**
-     * Set the mode fluently.
-     *
-     * @param int $mode
-     *
-     * @throws JSONLibraryException
-     *
-     * @return $this
-     */
-    public function mode($mode)
-    {
-        $acceptable_modes = [-1, 0, 1, 2];
-        if (!in_array($mode, $acceptable_modes)) {
-            throw new JSONLibraryException('bad_mode_set');
-        }
-        $this->mode = $mode;
-
-        return $this;
-    }
-
-    /**
-     * Handles quick functions for all known HTTP verbs.
-     *
-     * @param $name
-     * @param $arguments
-     *
-     * @throws JSONLibraryException
-     *
-     * @return Collections\Generic|\Illuminate\Support\Collection|void
-     */
-    public function __call($name, $arguments)
-    {
-        $map = [
-            0 => 'url',
-            1 => 'body',
-            2 => 'query',
-            3 => 'headers',
-        ];
-
-        /* @var string $url */
-        /* @var array $body */
-        /* @var array $query */
-        /* @var array $headers */
-        foreach ($map as $key => $value) {
-            $$value = null;
-            if (array_key_exists($key, $arguments)) {
-                $$value = $arguments[$key];
-            }
-        }
-
-        $allowed = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'];
-        $method = strtoupper($name);
-
-        if (!in_array($method, $allowed)) {
-            throw new JSONLibraryException('bad_method');
-        }
-
-        return $this->request($method, $url, $body, $query, $headers);
-    }
-
-    /**
-     * Setup empty arrays if null given.
-     *
-     * @param $body
-     * @param $query
-     * @param $headers
-     *
-     * @return array
-     */
-    private function setupVariables($body, $query, $headers)
-    {
-        if (is_null($headers)) {
-            $headers = [];
-        }
-        if (is_null($query)) {
-            $query = [];
-        }
-        if (is_null($body)) {
-            $body = [];
-        }
-
-        return [$body, $query, $headers];
+        return $response_body;
     }
 }
--- src/helpers.php
+++ src/helpers.php
@@ -15,8 +15,3 @@ function decode($body)
 
     return $array;
 }
-
-function has_string_keys(array $array)
-{
-    return count(array_filter(array_keys($array), 'is_string')) > 0;
-}
